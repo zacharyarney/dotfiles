d48175376905bd949a9f6650742decc0a5f19137
Zachary Arney <zachary.arney@gmail.com>
2024-01-10 22:41:44 -0800

    Adds check for stow in apply.sh

--- apply.sh
+++ apply.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+if command -v stow &> /dev/null
+then
 # iterates over directories calling stow for each one
-for FILE in */; 
-    do stow ${FILE%/};
-done
+    for FILE in */; 
+        do stow ${FILE%/};
+    done
+else
+    echo "GNU stow must be installed."
+fi
