6167b4f1550a4eaa2d110faf24abf12093f84926
Zachary Arney <zachary.arney@gmail.com>
2024-06-18 22:16:05 -0700

    mentor updates, telescope tmux config

--- nvim/.config/nvim/lua/zacharyarney/lazy/telescope.lua
+++ nvim/.config/nvim/lua/zacharyarney/lazy/telescope.lua
@@ -1,19 +1,28 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	-- or                              , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
 
-	config = function()
+    config = function()
         local opts = { noremap = true, silent = true }
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
-		vim.keymap.set('n', '<leader>gf', builtin.git_files, opts)
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
-		vim.keymap.set('n', '<leader>bb', builtin.buffers, opts)
-		vim.keymap.set('n', '<leader>fs', function()
-			builtin.grep_string({ search = vim.fn.input('Grep > ') })
-		end)
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
-	end
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } })
+        end, opts)
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, opts)
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+        vim.keymap.set('n', '<leader>bb', builtin.buffers, opts)
+        vim.keymap.set('n', '<leader>fs', function()
+            builtin.grep_string({ search = vim.fn.input('Grep > ') })
+        end)
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+        require('telescope').setup {
+            pickers = {
+                find_files = {
+                    hidden = true,
+                }
+            }
+        }
+    end
 }
-
