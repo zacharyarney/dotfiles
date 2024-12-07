12f413e55278cf685681344f0ec558037d5996ff
Zachary Arney <zachary.arney@gmail.com>
2024-12-06 17:06:48 -0800

    adds --apply to stow command and removes nvim spell

--- apply.sh
+++ apply.sh
@@ -4,7 +4,7 @@ if command -v stow &> /dev/null
 then
 # iterates over directories calling stow for each one
     for FILE in */; 
-        do stow ${FILE%/};
+        do stow --adopt ${FILE%/};
     done
 else
     echo "GNU stow must be installed."
